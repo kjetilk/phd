afd42e15407faed0a0c5c09c30b818eeeb11f056
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2012-03-19 09:59:12 +0000

    add more timing metrics
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@188 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- benchmark/dbpedia-in-R/curl-test.R
+++ benchmark/dbpedia-in-R/curl-test.R
@@ -5,9 +5,17 @@ library(RCurl)
 timeQuery <- function(endpoint, query, ..., curlh = getCurlHandle())
   {
     getForm(c(endpoint), query=query, ..., curl = curlh)
-    getCurlInfo(curlh)$starttransfer.time
+    info <- getCurlInfo(curlh)
+    list(
+         endpoint = info$total.time - info$namelookup.time,
+         connection = info$connect.time - info$namelookup.time,
+         firstbyte = info$starttransfer.time - info$connect.time,
+         alldata = info$total.time - info$starttransfer.time
+         )
+
+
   }
 
-timeQuery("http://dbpedia.org/sparql", "SELECT * WHERE { ?concept a skos:Concept } LIMIT 10");
+timeQuery("http://dbpedia.org/sparql", "SELECT * WHERE { ?concept a skos:Concept } LIMIT 100");
 
     
