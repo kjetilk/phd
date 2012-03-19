d31af1af91734df2766fd93c58f23550b8d62484
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2012-03-19 10:05:58 +0000

    indentation
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@189 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- benchmark/dbpedia-in-R/curl-test.R
+++ benchmark/dbpedia-in-R/curl-test.R
@@ -2,19 +2,16 @@ library(RCurl)
 
 #h = getCurlHandle()
 
-timeQuery <- function(endpoint, query, ..., curlh = getCurlHandle())
-  {
-    getForm(c(endpoint), query=query, ..., curl = curlh)
-    info <- getCurlInfo(curlh)
-    list(
-         endpoint = info$total.time - info$namelookup.time,
-         connection = info$connect.time - info$namelookup.time,
-         firstbyte = info$starttransfer.time - info$connect.time,
-         alldata = info$total.time - info$starttransfer.time
-         )
-
-
-  }
+timeQuery <- function(endpoint, query, ..., curlh = getCurlHandle()) {
+  getForm(c(endpoint), query=query, ..., curl = curlh)
+  info <- getCurlInfo(curlh)
+  list(
+       endpoint = info$total.time - info$namelookup.time,
+       connection = info$connect.time - info$namelookup.time,
+       firstbyte = info$starttransfer.time - info$connect.time,
+       alldata = info$total.time - info$starttransfer.time
+       )
+}
 
 timeQuery("http://dbpedia.org/sparql", "SELECT * WHERE { ?concept a skos:Concept } LIMIT 100");
 
