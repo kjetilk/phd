e08d39fd7f118cc35f574a0c43266a9942f8d1f7
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2012-03-22 13:01:57 +0000

    replace and create queries to run
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@194 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- benchmark/dbpedia-in-R/compileQueries.R
+++ benchmark/dbpedia-in-R/compileQueries.R
@@ -5,9 +5,11 @@ queries <- read.delim("/home/kjekje/DBPediaBenchmark/data/dbpedia.aksw.org/bench
 singleQuery <- function(endpoint, queries, runs = 5) {
   auxdata <- SPARQL(url=endpoint, query=queries$auxquery)
   using <- as.vector(auxdata$results[sample((1:nrow(auxdata$results)), runs),]) # TODO: Support multivars
-  using
+  sapply(using, insertAuxillaries, queries$query)
 }
 
-
+insertAuxillaries <- function(var, query) {
+  sub("%%var%%", paste("<", var, ">", sep="", collapse=""), query, fixed=TRUE)
+}
 
 singleQuery("http://dbpedia.org/sparql", queries[1,])
