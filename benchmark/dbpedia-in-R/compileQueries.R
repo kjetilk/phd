8d16ad77ff7e20448427c297dd79b554f965b86b
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2012-03-22 12:31:21 +0000

    running auxillary query
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@193 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- benchmark/dbpedia-in-R/compileQueries.R
+++ benchmark/dbpedia-in-R/compileQueries.R
@@ -1,11 +1,13 @@
 library(SPARQL)
 
-queries <- read.csv("/home/kjekje/DBPediaBenchmark/data/dbpedia.aksw.org/benchmark.dbpedia.org/Queries.txt", header=FALSE, col.names=c('name', 'query', 'auxquery'), row.names=1, sep="\t")
+queries <- read.delim("/home/kjekje/DBPediaBenchmark/data/dbpedia.aksw.org/benchmark.dbpedia.org/Queries.txt", header=FALSE, col.names=c('name', 'query', 'auxquery'), row.names=1, colClasses = "character")
 
 singleQuery <- function(endpoint, queries, runs = 5) {
   auxdata <- SPARQL(url=endpoint, query=queries$auxquery)
-  using <- auxdata$results[sample((1:nrow(auxdata$results)), runs),]
+  using <- as.vector(auxdata$results[sample((1:nrow(auxdata$results)), runs),]) # TODO: Support multivars
   using
 }
+
+
 
 singleQuery("http://dbpedia.org/sparql", queries[1,])
