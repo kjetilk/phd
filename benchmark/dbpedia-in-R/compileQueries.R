d585dedf659fe7f1d11fd6e1443439733da5594f
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2012-03-28 18:50:23 +0000

    the error was in the SPARQL client
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@198 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- benchmark/dbpedia-in-R/compileQueries.R
+++ benchmark/dbpedia-in-R/compileQueries.R
@@ -4,17 +4,16 @@ queries <- read.delim("/home/kjekje/DBPediaBenchmark/data/dbpedia.aksw.org/bench
 
 singleQuery <- function(endpoint, queries, runs = 5) {
   auxdata <- SPARQL(url=endpoint, query=queries$auxquery)
-#  browser()
   using <- as.matrix(auxdata$results[sample((1:nrow(auxdata$results)), runs),])
   runqueries <- apply(using, 1, runQueries, queries$query, endpoint)
   runqueries
 }
 
 runQueries <- function(var, queryWithVar, endpoint) {
-#  browser()
   query <- sub("%%var%%", paste("<", var, ">", sep="", collapse=""), queryWithVar, fixed=TRUE)
   timeQuery(endpoint, query)
 }
 
 single <- singleQuery("http://kjekje-vm/sparql", queries["union,distinct",])
+single1 <- singleQuery("http://kjekje-vm/sparql", queries[1,])
 single <- singleQuery("http://dbpedia.org/sparql", queries["union,distinct",])
