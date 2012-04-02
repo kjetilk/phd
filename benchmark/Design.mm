9fdd890e6f1b7adc7f50c06f6c2117288840575a
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2012-04-02 21:12:51 +0000

    More points
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@203 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- benchmark/Design.mm
+++ benchmark/Design.mm
@@ -1,10 +1,22 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1333395561680" ID="ID_485928455" MODIFIED="1333395581393" TEXT="Benchmark design">
-<node CREATED="1333395586724" ID="ID_1100110923" MODIFIED="1333395625835" POSITION="right" TEXT="Exhaustive macrobenchmarks"/>
+<node CREATED="1333395586724" ID="ID_1100110923" MODIFIED="1333395625835" POSITION="right" TEXT="Exhaustive macrobenchmarks">
+<node CREATED="1333396467429" ID="ID_1279958215" MODIFIED="1333396476361" TEXT="Maybe enough for some?"/>
+<node CREATED="1333396477376" ID="ID_36273484" MODIFIED="1333396499109" TEXT="Use to drill down to microbenchmarks"/>
+<node CREATED="1333396516793" ID="ID_839664789" MODIFIED="1333396535880" TEXT="Core problem: How do I cover everything?">
+<node CREATED="1333396547689" ID="ID_1380436799" MODIFIED="1333396556448" TEXT="Quantify degree of coverage?"/>
+<node CREATED="1333396595698" ID="ID_1139628688" MODIFIED="1333396739290" TEXT="Combine qualitative with quantitative?"/>
+</node>
+</node>
 <node CREATED="1333395631652" ID="ID_1757863365" MODIFIED="1333395641497" POSITION="right" TEXT="Complexity bounds"/>
 <node CREATED="1333395642148" ID="ID_750759169" MODIFIED="1333395740740" POSITION="right" TEXT="Many functions, log x, x log x, x&#xb2;, x! etc"/>
 <node CREATED="1333395750781" ID="ID_2697894" MODIFIED="1333395811084" POSITION="right" TEXT="Exposure of coefficients"/>
-<node CREATED="1333395811998" ID="ID_1684541291" MODIFIED="1333395811998" POSITION="right" TEXT=""/>
+<node CREATED="1333395811998" ID="ID_1684541291" MODIFIED="1333396919461" POSITION="right" TEXT="End results:">
+<node CREATED="1333396921343" ID="ID_267123421" MODIFIED="1333396950265" TEXT="Benchmarks for thorough evaluation"/>
+<node CREATED="1333396951055" ID="ID_1178442085" MODIFIED="1333396968582" TEXT="Help developers evaluate a change"/>
+<node CREATED="1333396969647" ID="ID_1644888900" MODIFIED="1333397096151" TEXT="Assist people in choosing engine by make it easier to articulate what&apos;s important"/>
+<node CREATED="1333397101514" ID="ID_39116573" MODIFIED="1333397109983" TEXT="Expose data that can help optimization"/>
+</node>
 </node>
 </map>
