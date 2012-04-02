c82e6862a88e8b8d9bd735714ccf06ddd8ba2c95
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2012-04-02 19:53:39 +0000

    Start benchmark design mind map
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@202 43ef1190-90fd-4dd7-a769-d0121f02b80e

---
+++ benchmark/Design.mm
@@ -0,0 +1,10 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1333395561680" ID="ID_485928455" MODIFIED="1333395581393" TEXT="Benchmark design">
+<node CREATED="1333395586724" ID="ID_1100110923" MODIFIED="1333395625835" POSITION="right" TEXT="Exhaustive macrobenchmarks"/>
+<node CREATED="1333395631652" ID="ID_1757863365" MODIFIED="1333395641497" POSITION="right" TEXT="Complexity bounds"/>
+<node CREATED="1333395642148" ID="ID_750759169" MODIFIED="1333395740740" POSITION="right" TEXT="Many functions, log x, x log x, x&#xb2;, x! etc"/>
+<node CREATED="1333395750781" ID="ID_2697894" MODIFIED="1333395811084" POSITION="right" TEXT="Exposure of coefficients"/>
+<node CREATED="1333395811998" ID="ID_1684541291" MODIFIED="1333395811998" POSITION="right" TEXT=""/>
+</node>
+</map>
