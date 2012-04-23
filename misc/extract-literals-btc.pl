d1e2f34293a419a9f4ff74d1493b7a063552fed0
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2012-04-23 14:34:05 +0000

    *** empty log message ***
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@235 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- misc/extract-literals-btc.pl
+++ misc/extract-literals-btc.pl
@@ -6,6 +6,7 @@ use warnings;
 use PerlIO::gzip;
 
 open (COMQ, "<:gzip", $ARGV[0]);
+print STDERR "Extracting literals from $ARGV[0]";
 while (<COMQ>) {
 	my ($lit) = m/\"(.+)\"/;
 	print "$lit\n" if ($lit);
