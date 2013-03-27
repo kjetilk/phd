da701d5fbf67d81bb4b09b8330b8eae48e15a56b
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-03-27 14:59:05 +0000

    a bit of cargo-culting
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@427 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- exp1/run.R
+++ exp1/run.R
@@ -1,7 +1,19 @@
-sparqlEval <- function(design) {
+sparqlEval <- function(design, dir = "experiment/") {
   if (!inherits(design, "design")) {
     stop("Argument 'design' has to be a design object or an object of a subclass")
   }
+  # First get all from files
+  lapply(factor.names(design), function(factorname) {
+    name <- names(factorname)
+    names <- sapply(factorname, function(level) {
+      print name
+      cat(name, "-", level, sep='')
+    }
+    , name )
+#    content <- readLines(cat(dir, 
+    stop(names)
+  } )
+  # Run the experiment by iterating design matrix
   apply(design, 1, experiment)
 
 }
