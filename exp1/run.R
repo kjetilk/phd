f9af8fcf7bfc393ddcfce0996932623aed9954db
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-04-12 15:26:24 +0000

    flawed for loop
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@429 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- exp1/run.R
+++ exp1/run.R
@@ -1,20 +1,21 @@
-sparqlEval <- function(design, dir = "experiment/") {
+sparqlEval <- function(design, path = "experiment/") {
   if (!inherits(design, "design")) {
     stop("Argument 'design' has to be a design object or an object of a subclass")
   }
-  # First get all from files
-  lapply(factor.names(design), function(factorname) {
-    name <- names(factorname)
-    names <- sapply(factorname, function(level) {
-      print name
-      cat(name, "-", level, sep='')
+  allnames <- factor.names(design)
+  files <- allnames 
+  # Use a for loop to read the files, though rapply is probably the R way
+  for(name in names(allnames)) {
+    for(j in 1:length(allnames[[name]])) {
+      files[[name]][j] <- cat(path,names(allnames)[name], "-", allnames[[name]][j], sep="")
     }
-    , name )
-#    content <- readLines(cat(dir, 
-    stop(names)
-  } )
+  }
+
+  files
+
+
   # Run the experiment by iterating design matrix
-  apply(design, 1, experiment)
+  #apply(design, 1, experiment)
 
 }
 
