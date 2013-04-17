82e139fa6cf8551c0ed77bf542c94b18a7b39907
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-04-17 20:58:25 +0000

    quite working endpoint code
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@449 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- exp1/run.R
+++ exp1/run.R
@@ -10,26 +10,32 @@ sparqlEval <- function(design, path = "experiment/") {
                   factor=facnames, justnames=names(facnames))
   # Run the experiment by iterating design matrix
   experiments <- apply(design, 1, experiment, files=files)
-  experiments
+  cbind(design, experiments)
 }
 
 loadexperiment <-  function(level, name, path) {
   filename <- paste(path, name, "-", level, sep="")
   expfile <- readLines(filename)
   query <- FALSE
-  endpoint <- FALSE
+  endpointhost <- FALSE
+  endpointportpart <- FALSE
   type <- NA
   if(!(is.na(charmatch("# SPARQL", expfile[1])))) {
     query <- TRUE
     type <- substr(expfile[1], 10, 20)
   }
-  else if(!(is.na(charmatch("# ENDPOINT URL", expfile[1])))) {
-    endpoint <- TRUE
+  else if(!(is.na(charmatch("# ENDPOINT URL HOST", expfile[1])))) {
+    endpointhost <- TRUE
+    type <- "HOST"
+  }
+    else if(!(is.na(charmatch("# ENDPOINT URL PORTPART", expfile[1])))) {
+    endpointportpart <- TRUE
+    type <- "PORTPART"
   }
   else {
     stop("No valid file header (SPARQL / ENDPOINT URL) found")
   }
-  list(content = expfile, query = query, endpoint = endpoint, type = type, level = level)
+  list(content = expfile, query = query, endpointhost = endpointhost, endpointportpart=endpointportpart, type = type, level = level)
   
 }
 
@@ -41,17 +47,27 @@ experiment <- function(run, files) {
     cat("Previous Endpoint URL", endpointurl, "returned error\n")
     endpointurl <- ask("Please enter valid endpoint URL: ")
   }
-  ret <- apply(cbind(run, files), 1, compose)
-  query <- paste("PREFIX dbo: <http://dbpedia.org/ontology/> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX foaf: <http://xmlns.com/foaf/0.1/> PREFIX dbpprop: <http://dbpedia.org/property/> PREFIX skos: <http://www.w3.org/2004/02/skos/core#> PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> SELECT * WHERE {", paste(unlist(ret[!is.na(ret)]), collapse=" "), "}")
+  thisexperiment <- cbind(run, files)
+  # One pass should really suffice here
+  querypattern <- apply(thisexperiment, 1, compose, type="query")
+  ret <- apply(thisexperiment, 1, compose, type="endpointhost")
+  host <- ret[!is.na(ret)]
+  if(length(host) != 1) {
+    stop("We need exactly one factor with hostname")
+  }
+  ports <- apply(thisexperiment, 1, compose, type="endpointportpart")
+  endpointurl <- paste('http://', host, ":80", paste(unlist(ports[!is.na(ports)]), collapse=""), "/sparql", sep="")
+
+  query <- paste("PREFIX dbo: <http://dbpedia.org/ontology/> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX foaf: <http://xmlns.com/foaf/0.1/> PREFIX dbpprop: <http://dbpedia.org/property/> PREFIX skos: <http://www.w3.org/2004/02/skos/core#> PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> SELECT * WHERE {", paste(unlist(querypattern[!is.na(querypattern)]), collapse=" "), "}")
+
   timeQuery(endpointurl, query)$endpoint
 }
 
-compose <- function(factor) {
+compose <- function(factor, type) {
   file <- factor$files[,unlist(factor$files["level",]) == factor$run]
-  if(file$query) {
+  if(file[[type]]) {
     file$content[-1]
   } else {
     NA
   }
 }
-
