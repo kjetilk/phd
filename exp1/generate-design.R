ca5d5e5c06d06d5b8cfd0ac44cd9957714186f5a
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-04-25 22:08:38 +0000

    Add completely random design
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@458 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- exp1/generate-design.R
+++ exp1/generate-design.R
@@ -11,3 +11,6 @@ sparqltest.sans.machine.design <- cross.design(
                                   fac.design(nlevels = 2, nfactors = 5,
                                              factor.names = c("BGPComp", "Lang", "Range", "Union", "Optional")),
                                   randomize = F)
+
+sparqltest.random.design <- fac.design(nlevels = 2, nfactors = 8,
+                                             factor.names = c("Implement", "TripleC", "Machine", "BGPComp", "Lang", "Range", "Union", "Optional"))
