98a0fffbfbcb59fc83620fc0236a610e14a686fa
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-04-26 06:38:36 +0000

    Check if the port responds on startup
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@459 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- exp1/run.sh
+++ exp1/run.sh
@@ -13,4 +13,14 @@ bin/4s-backend smallsleep
 bin/4s-httpd -s -1 -p 8022 largesleep
 bin/4s-httpd -s -1 -p 8021 smallsleep
 ps ux | grep 4s
+
+echo 8012
+wget -O /dev/null http://localhost:8012/status/
+echo 8011
+wget -O /dev/null http://localhost:8011/status/
+echo 8022
+wget -O /dev/null http://localhost:8022/status/
+echo 8021
+wget -O /dev/null http://localhost:8021/status/
+
 exit 0
