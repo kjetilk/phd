abbf52b0c69af39431ee3291d31fc08915d85229
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-05-02 14:20:18 +0000

    Analysis with two-sample t-test
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@487 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- exp1/analysis.R
+++ exp1/analysis.R
@@ -5,3 +5,18 @@ coef.sorted <- function(model) {
 implementation.coef <- function(model) {
   coef(model)[grep("Implement", names(coef(model)))]
 }
+
+fulllm <- function(results) {
+  lm(formula = experiments ~ Implement * TripleC * BGPComp * Lang * Range * Union * Optional * Machine, data = results)
+}
+
+robust.single <- function(model, control = "Implement", noise = c("TripleC", "Lang", "Union", "Optional"), ...) {
+  fm <- as.formula(paste("experiments ~", control, " * ", paste(noise, collapse=" * ")));
+  allmeans <- aggregate(fm, data=model, mean)
+  t.test(
+         allmeans[allmeans[[control]] == 2,"experiments"],
+         allmeans[allmeans[[control]] == 1,"experiments"],
+         alternative="less",
+         ...)
+}
+  
