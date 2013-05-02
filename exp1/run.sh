3ecddb59635b7349935da4932f54292e1c36eed7
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-05-02 08:02:46 +0000

    switch sleep and unmod for more intuitive analysis
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@483 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- exp1/run.sh
+++ exp1/run.sh
@@ -4,14 +4,14 @@ killall 4s-backend
 killall 4s-httpd
 cd 4store-unmodified/
 bin/4s-backend largeunmod
-bin/4s-httpd -s -1 -p 8012 largeunmod
+bin/4s-httpd -s -1 -p 8022 largeunmod
 bin/4s-backend smallunmod
-bin/4s-httpd -s -1 -p 8011 smallunmod
+bin/4s-httpd -s -1 -p 8021 smallunmod
 cd ../4store-sleep/
 bin/4s-backend largesleep
 bin/4s-backend smallsleep
-bin/4s-httpd -s -1 -p 8022 largesleep
-bin/4s-httpd -s -1 -p 8021 smallsleep
+bin/4s-httpd -s -1 -p 8012 largesleep
+bin/4s-httpd -s -1 -p 8011 smallsleep
 ps ux | grep 4s
 
 echo 8012
