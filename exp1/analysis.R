db59b1b2bb35858697f2027585a6ec245925efc7
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-05-02 16:23:44 +0000

    trivial logic bug
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@489 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- exp1/analysis.R
+++ exp1/analysis.R
@@ -10,13 +10,14 @@ fulllm <- function(results) {
   lm(formula = experiments ~ Implement * TripleC * BGPComp * Lang * Range * Union * Optional * Machine, data = results)
 }
 
-robust <- function(model, control = "Implement", noise = c("TripleC", "Lang", "Union", "Optional"), pairwise=TRUE, ...) {
+robust <- function(model, control = "Implement", noise = c("TripleC", "Lang", "Union", "Optional"), pairwise=FALSE, ...) {
   fm <- as.formula(paste("experiments ~", control, " * ", paste(noise, collapse=" * ")));
   allmeans <- aggregate(fm, data=model, mean)
   oldimplement <- allmeans[allmeans[[control]] == 1,"experiments"]
   newimplement <- allmeans[allmeans[[control]] == 2,"experiments"]
   if(pairwise) {
+  } else {
     t.test(newimplement, oldimplement, alternative="less", ...)
-  }# else {
+  }
 }
   
