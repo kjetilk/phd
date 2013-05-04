68b42253238818e14a561793289df9b713656c0d
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-05-04 18:50:51 +0000

    Support running huge too
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@494 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- exp1/run.sh
+++ exp1/run.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+if [ $1 ] 
+  then
+	largename=$1
+  else
+	largename="large"
+fi
+
+
 killall 4s-backend 
 killall 4s-httpd
 cd 4store-unmodified/
-bin/4s-backend largeunmod
-bin/4s-httpd -s -1 -p 8022 largeunmod
+bin/4s-backend $largename"unmod"
+bin/4s-httpd -s -1 -p 8022 $largename"unmod"
 bin/4s-backend smallunmod
 bin/4s-httpd -s -1 -p 8021 smallunmod
 cd ../4store-sleep/
-bin/4s-backend largesleep
+bin/4s-backend $largename"sleep"
 bin/4s-backend smallsleep
-bin/4s-httpd -s -1 -p 8012 largesleep
+bin/4s-httpd -s -1 -p 8012 $largename"sleep"
 bin/4s-httpd -s -1 -p 8011 smallsleep
 ps ux | grep 4s
 
