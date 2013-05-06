29c2c0b8d5521786299d29e7fa4a90e53ca869e4
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-05-06 13:07:01 +0000

    Working simple t-test
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@511 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- exp1/analysis.R
+++ exp1/analysis.R
@@ -10,10 +10,19 @@ fulllm <- function(results) {
   lm(formula = experiments ~ Implement * TripleC * BGPComp * Lang * Range * Union * Optional * Machine, data = results)
 }
 
-robust <- function(model, control = "Implement", inactive = c("Machine", "BGPComp", "Range"), pairwise=FALSE, ...) {
-  fm <- as.formula(paste("experiments ~", control, " * ", paste(inactive, collapse=" * ")));
+robust <- function(model, control = "Implement",
+                   noise = c("TripleC", "Lang", "Union", "Optional"), 
+                   inactive = c("Machine", "BGPComp", "Range"),
+                   pairwise=FALSE, ...) {
+#  fm <- as.formula(paste("experiment~", paste(noise, collapse=" * ")))
+  fm <- as.formula(paste("experiments ~", control, " * ", paste(inactive, collapse=" * ")))
+#rowser()
+#  ddply(model, fm, function(subset) {
+#    browser()
+#  }
+#    )
   allmeans <- aggregate(fm, data=model, mean)
-  allmeans
+  t.test(allmeans[allmeans["Implement"] == 1,]$experiments, allmeans[allmeans["Implement"] == 2,]$experiments)
 
 }
   
