fcda87ed11dc5876f292d9d680a550f71e12388e
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-05-08 14:39:16 +0000

    generate some figures
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@530 43ef1190-90fd-4dd7-a769-d0121f02b80e

---
+++ exp1/generate-figures.R
@@ -0,0 +1,27 @@
+library(FrF2)
+library(plyr)
+
+load("RelevantExperiments.RData")
+
+source("analysis.R")
+
+postscript(file="fullnormal.eps")
+DanielPlot(runbgplangf1.lm)
+dev.off()
+
+postscript(file="fulllenth.eps")
+LenthPlot(runbgplangf1.lm)
+dev.off()
+
+postscript(file="frac32normal.eps")
+DanielPlot(frac32r1.lm)
+dev.off()
+
+postscript(file="frac64normalalpha005.eps")
+DanielPlot(frac64r3.lm)
+dev.off()
+
+postscript(file="frac64normalalpha015l.eps")
+DanielPlot(frac64r3.lm, alpha=0.15)
+dev.off()
+
