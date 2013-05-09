d8e741680d24329945135641f50d67c463b4019a
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-05-09 22:38:18 +0000

    Changes to the generated figures
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@551 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- exp1/generate-figures.R
+++ exp1/generate-figures.R
@@ -5,22 +5,25 @@ load("RelevantExperiments.RData")
 
 source("analysis.R")
 
+
 pdf(file="fullnormal.pdf")
-DanielPlot(runbgplangf1.lm)
+par(mai=c(0.6,0.6,0.05,0.05))
+DanielPlot(runbgplangf1.lm, main="", cex.fac=0.8)
 dev.off()
 
-pdf(file="frac32normal.pdf")
-DanielPlot(frac32r1.lm)
+pdf(file="frac32normal.pdf", height=3)
+par(mai=c(0.6,0.6,0.05,0.05))
+DanielPlot(frac32r1.lm, main="", cex.fac=0.8)
 dev.off()
 
-pdf(file="frac64normalalpha005.pdf")
-DanielPlot(frac64r3.lm)
+pdf(file="frac64normal.pdf", height=5)
+par(mai=c(0.6,0.6,0.05,0.05))
+DanielPlot(frac64r3.lm, alpha=c(0.05, 0.15), main="", cex.fac=0.8)
 dev.off()
 
-pdf(file="frac64normalalpha015.pdf")
-DanielPlot(frac64r3.lm, alpha=0.15)
-dev.off()
 
 pdf(file="frac64hugenormal.pdf")
-DanielPlot(frac64huger1.lm)
+par(mai=c(0.6,0.6,0.05,0.05))
+DanielPlot(frac64huger1.lm, main="", cex.fac=0.8)
 dev.off()
+
