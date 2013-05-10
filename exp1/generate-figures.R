e228bf6870a5fa6df6c968b4baea0620e937abe0
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-05-10 19:47:56 +0000

    Figures could be a little larger
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@575 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- exp1/generate-figures.R
+++ exp1/generate-figures.R
@@ -17,13 +17,13 @@ par(mai=c(0.6,0.6,0.05,0.05))
 DanielPlot(frac32r1.lm, main="", cex.fac=0.8)
 dev.off()
 
-pdf(file="frac64normal.pdf", height=5)
+pdf(file="frac64normal.pdf", height=6)
 par(mai=c(0.6,0.6,0.05,0.05))
 DanielPlot(frac64r3.lm, alpha=c(0.05, 0.15), main="", cex.fac=0.8)
 dev.off()
 
 
-pdf(file="frac64hugenormal.pdf", height=5.5)
+pdf(file="frac64hugenormal.pdf", height=6)
 par(mai=c(0.6,0.6,0.05,0.05))
 DanielPlot(frac64huger1.lm, main="", cex.fac=0.8)
 dev.off()
