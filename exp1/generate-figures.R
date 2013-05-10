47b32391a78a28f84d2d1dfc7957886fa4f737a3
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2013-05-10 17:09:30 +0000

    fixing the size of figures
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@568 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- exp1/generate-figures.R
+++ exp1/generate-figures.R
@@ -23,7 +23,7 @@ DanielPlot(frac64r3.lm, alpha=c(0.05, 0.15), main="", cex.fac=0.8)
 dev.off()
 
 
-pdf(file="frac64hugenormal.pdf")
+pdf(file="frac64hugenormal.pdf", height=5.5)
 par(mai=c(0.6,0.6,0.05,0.05))
 DanielPlot(frac64huger1.lm, main="", cex.fac=0.8)
 dev.off()
