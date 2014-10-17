92fc1e08021539cba1fe748d67f8e2f688a76b08
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2014-10-17 15:10:31 +0000

    getting the endpoint URLs ok
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@778 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- sanity/endpoints.pl
+++ sanity/endpoints.pl
@@ -4,9 +4,10 @@ use utf8;
 use warnings;
 binmode(STDOUT, ":utf8");
 use Data::Dumper;
-
+use URI::Escape;
 use URI;
 use Web::Scraper;
+
 my $rows = scraper {
   process "tr", "rows[]" => scraper {
     process "td > img" , status => '@src';
@@ -16,13 +17,26 @@ my $rows = scraper {
   };
 };
 
-my $res = $rows->scrape( URI->new("http://sparqles.okfn.org/availability") );
+#my $res = $rows->scrape( URI->new("http://sparqles.okfn.org/availability") );
+my $res = $rows->scrape( URI->new("file:///home/kjetil/Projects/SemWeb/PhD/sanity/SPARQL\ Endpoints\ Status.html") );
 
-my $i = 0;
+#die Dumper($res);
+
+my $total = 0;
+my $skipped = 0;
+my $timedout = 0;
 foreach my $row (@{$res->{rows}}) {
-	$i++;
+	next unless ($row->{'status'});
+	$total++;
 	warn Dumper($row);
-die "Finished" if ($i>5);
+	if ($row->{'status'}->as_string =~ m/gray/) {
+		$skipped++;
+		next;
+	}
+	my ($raw) = $row->{'link'}->as_string =~ m!^http://sparqles.okfn.org/endpoint/(\S+)$!;
+	my $url = uri_unescape($raw);
+	print $url . "\n";
+	die "Finished" if ($total>5);
 }
 
 1;
