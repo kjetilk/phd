77d3fb0a73c2c966c0bf617ec8957a3f5d641e2f
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2014-11-28 13:22:20 +0000

    Rewrite to emit an RDF hitlist
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@821 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- sanity/hitlist.pl
+++ sanity/hitlist.pl
@@ -7,18 +7,24 @@ use Text::CSV;
 use Data::Dumper;
 use RDF::Query::Client;
 use URI;
+use RDF::Trine qw(statement iri literal);
+use URI::NamespaceMap;
+
+my $nm = URI::NamespaceMap->new(['dct']);
+my $dct = $nm->namespace_uri('dct');
+my $om = RDF::Trine::Model->temporary_model;
+
 
 print STDERR "Reading prefix.cc URLs\n";
 
 my $csv = Text::CSV->new ( { binary => 1 } )  # should set binary attribute.
                 or die "Cannot use CSV: ".Text::CSV->error_diag ();
 
-my %hitlist;
 open my $fh, "<:encoding(utf8)", "/home/kjetil/Projects/SemWeb/data/all.file.csv" or die "all.file.csv: $!";
 while ( my $row = $csv->getline( $fh ) ) {
 	my $url = $row->[1];
-	$hitlist{$url}{'source'} = ['prefix.cc'];
-	$hitlist{$url}{'target'} = ['vocabulary'];
+	$om->add_statement(statement(iri($url), iri($dct->source), iri('http://prefix.cc/')));
+	$om->add_statement(statement(iri($url), iri($dct->type), literal('vocabulary')));
 }
 
 $csv->eof or $csv->error_diag();
@@ -33,18 +39,15 @@ my $iterator = $query->execute('http://lov.okfn.org/endpoint/lov');
 while (my $row = $iterator->next) {
    my $url1 = URI->new($row->{vocabURI}->as_string);
    my $url2 = URI->new($row->{nsURI}->as_string);
-	if ($hitlist{"$url2"}) {
-		push(@{$hitlist{"$url2"}{'source'}}, 'lov');
-	} else {
-		$hitlist{"$url2"}{'source'} = ['lov'];
-		$hitlist{"$url2"}{'target'} = ['vocabulary'];
-	}
+	$om->add_statement(statement(iri($url2), iri($dct->source), iri('http://lov.okfn.org/')));
+	$om->add_statement(statement(iri($url2), iri($dct->type), literal('vocabulary')));
 	my $url3 = URI->new($url1->as_string . '#');
 	unless ($url2->eq($url1) || $url2->eq($url3)) {
-		$hitlist{"$url2"}{'tryalso'} = "$url1";
+		$om->add_statement(statement(iri($url2), iri($dct->identifier), iri($url1)));
 	}
 }
 
 
-
-print Dumper(\%hitlist);
+print STDERR "Serializing the results\n";
+my $ser = RDF::Trine::Serializer->new('turtle', namespaces => { 'dct' => $dct->uri->as_string });
+print $ser->serialize_model_to_string($om);
