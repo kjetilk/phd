5bb0a97b288e28c96ca8eab55ebf2a975e54e0fe
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2014-12-05 12:51:37 +0000

    bugfixing
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@846 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- sanity/fetcher.pl
+++ sanity/fetcher.pl
@@ -14,21 +14,22 @@ my @files = ('/mnt/ssdstore/data/btc-processed/hitlist-test.ttl');
 my $tp = RDF::Trine::Parser->new('turtle');
 
 my $data;
-foreach my $filename (@files) {
-	$tp->parse_file('', $filename, \&thandler);
-}
 
 my $thandler = sub {
 	my $st = shift;
 	my $suri = URI->new($st->subject->uri_value);
 	my $host = $suri->host;
-	if ($st->predicate->equals($dct->source)) {
+	if ($st->predicate->equal($dct->source)) {
 		push(@{$data->{$host}->{$suri}->{source}}, $st->object->uri_value);
-	} elsif ($st->predicate->equals($dct->type)) {
+	} elsif ($st->predicate->equal($dct->type)) {
 		push(@{$data->{$host}->{$suri}->{type}}, $st->object->literal_value);
-	} elsif ($st->predicate->equals($dct->identifier)) {
+	} elsif ($st->predicate->equal($dct->identifier)) {
 		$data->{$host}->{$suri}->{alternate} = $st->object->uri_value;
 	}
+};
+
+foreach my $filename (@files) {
+	$tp->parse_file('http://invalid/', $filename, $thandler);
 }
 
 print Dumper($data);
