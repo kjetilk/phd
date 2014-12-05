8e816f0922cfffa9ded2cb8f6b18b676a491a81f
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2014-12-05 15:25:33 +0000

    First working fetcher
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@847 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- sanity/fetcher.pl
+++ sanity/fetcher.pl
@@ -3,11 +3,23 @@
 use strict;
 use warnings;
 use Data::Dumper;
+use Progress::Any::Output;
+Progress::Any::Output->set('TermProgressBarColor');
+use Progress::Any;
 
 use RDF::Trine;
 
+my $prfetch = Progress::Any->get_indicator(
+        task => "fetching");
+my $prparse = Progress::Any->get_indicator(
+        task => "fetching");
+
+$prparse->update(message => "Setting up");
+
+
 my $dct = RDF::Trine::Namespace->new('http://purl.org/dc/terms/');
 
+my $writedir = '/mnt/ssdstore/data/btc-processed/crawl/';
 
 my @files = ('/mnt/ssdstore/data/btc-processed/hitlist-test.ttl');
 
@@ -29,7 +41,51 @@ my $thandler = sub {
 };
 
 foreach my $filename (@files) {
+	$prparse->update(message => "Parsing $filename");
 	$tp->parse_file('http://invalid/', $filename, $thandler);
 }
 
-print Dumper($data);
+$prfetch->update(message => "Initializing UA");
+
+use Parallel::ForkManager;
+my $pm = Parallel::ForkManager->new(4);
+
+use LWP::UserAgent;
+use HTTP::Request;
+
+my $ua = LWP::UserAgent->new;
+$ua->agent('Semweb cache project (see http://folk.uio.no/kjekje/ ) ');
+$ua->max_size( 1000000 );
+$ua->timeout( 20 );
+
+my @hosts = keys %{$data};
+$prfetch->target(scalar @hosts);
+
+DATA_LOOP:
+foreach my $host (@hosts) {
+  my $uricount = scalar keys %{$data->{$host}};
+  $prfetch->update(message => "Fetching $uricount resources from $host");
+  # Forks and returns the pid for the child:
+  my $pid = $pm->start and next DATA_LOOP;
+  open (my $fh, '>>', $writedir . $host) || die "Couldn't open $writedir for writing";
+  while ((my $uri, my $details) = each(%{$data->{$host}})) {
+	  my $request = HTTP::Request->new(GET => $uri);
+	  if ($details->{type} eq 'endpoint') {
+		  $request->uri( $uri . 'SELECT DISTINCT * WHERE {	?s a ?class . } LIMIT 10');
+		  $request->header( Accept => '*/*' );
+	  } else {
+		  $request->header( Accept => RDF::Trine::Parser::default_accept_header );
+	  }
+	  my $firstresponse = $ua->request( $request );
+	  if ($firstresponse->is_success) {
+		  my $etag = $firstresponse->header('ETag');
+		  print $fh $uri . "\t" . $etag . "\n" if ($etag);
+	  }
+	  close $fh;
+	  sleep 5 if ($uricount > 1);
+  }
+  $pm->finish; # Terminates the child process
+}
+
+$prfetch->finish;
+$prparse->finish;
