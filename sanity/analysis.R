4da56de901a927cf0f398f073eb67dba1f63e41a
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2014-12-23 14:55:03 +0000

    Some R code to query local endpoint
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@917 43ef1190-90fd-4dd7-a769-d0121f02b80e

---
+++ sanity/analysis.R
@@ -0,0 +1,5 @@
+
+sparqlfile <- function(filename, dir = "/home/kjetil/sanity/queries/", url = "http://robin:8890/sparql" , ...) {
+    query <- paste(readLines(paste0(dir, filename)), collapse="\n")
+    SPARQL(url = url, query = query, ...)
+}
