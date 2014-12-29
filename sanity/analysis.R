3868c7b5d8e4ef937a0048f93e54ce965d93fd8e
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2014-12-29 16:36:19 +0000

    More analysis
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@918 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- sanity/analysis.R
+++ sanity/analysis.R
@@ -1,4 +1,6 @@
 
+library(SPARQL)
+
 sparqlfile <- function(filename, dir = "/home/kjetil/sanity/queries/", url = "http://robin:8890/sparql" , ...) {
     query <- paste(readLines(paste0(dir, filename)), collapse="\n")
     SPARQL(url = url, query = query, ...)
