c3e4294dbcfe7cb5740401384fb4ef64a2277eea
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2015-01-02 20:36:05 +0000

    First analysis of Server header with Hard
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@929 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- sanity/queries.R
+++ sanity/queries.R
@@ -1,1 +1,5 @@
 checkserver <- sparqlfile("check-server.rq")
+reshardbin <- as.integer(checkserver$results$hard)
+reshardbin[! is.na(reshardbin)] <- 1
+reshardbin[is.na(reshardbin)] <- 0
+chisq.test(checkserver$results$server, reshardbin, simulate.p.value=T, B = 10000)
