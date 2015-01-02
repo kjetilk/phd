66892a8d8642fea98e443dbc81348158a7d672ff
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2015-01-02 20:46:24 +0000

    Test for Server with promise
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@930 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- sanity/queries.R
+++ sanity/queries.R
@@ -3,3 +3,8 @@ reshardbin <- as.integer(checkserver$results$hard)
 reshardbin[! is.na(reshardbin)] <- 1
 reshardbin[is.na(reshardbin)] <- 0
 chisq.test(checkserver$results$server, reshardbin, simulate.p.value=T, B = 10000)
+
+respromisebin <- checkserver$results$promise
+respromisebin[! is.na(respromisebin)] <- 1
+respromisebin[is.na(respromisebin)] <- 0
+chisq.test(checkserver$results$server, respromisebin, simulate.p.value=T, B = 10000)
