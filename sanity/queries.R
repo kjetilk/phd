853369021fac7c00b7faef4bdbd0a6d516fddee3
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2015-01-04 23:35:59 +0000

    Get the proportions of cached
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@933 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- sanity/queries.R
+++ sanity/queries.R
@@ -8,3 +8,7 @@ respromisebin <- checkserver$results$promise
 respromisebin[! is.na(respromisebin)] <- 1
 respromisebin[is.na(respromisebin)] <- 0
 chisq.test(checkserver$results$server, respromisebin, simulate.p.value=T, B = 10000)
+
+checkservercount <- sparqlfile("check-server-count.rq")
+serverratio <- data.frame(name=checkservercount$results$server, promise=checkservercount$results$pc/checkservercount$results$sc, hard=checkservercount$results$hc/checkservercount$results$sc, pc=checkservercount$results$pc, hc=checkservercount$results$hc)
+xtable(serverratio[which(serverratio$hard == 1), ]$name)
