5dd4bb032470b2b1e0f6adbf3253fedec8905f12
kjekje <kjekje@43ef1190-90fd-4dd7-a769-d0121f02b80e>
2015-01-05 15:59:11 +0000

    Stats for all fresh
    git-svn-id: https://svn.ifi.uio.no/repos/users/kjekje-phd@941 43ef1190-90fd-4dd7-a769-d0121f02b80e

--- sanity/queries.R
+++ sanity/queries.R
@@ -13,3 +13,14 @@ checkservercount <- sparqlfile("check-server-count.rq")
 serverratio <- data.frame(name=checkservercount$results$server, promise=checkservercount$results$pc/checkservercount$results$sc, hard=checkservercount$results$hc/checkservercount$results$sc, pc=checkservercount$results$pc, hc=checkservercount$results$hc)
 xtable(as.matrix(serverratio[which(serverratio$hard == 1), ]$name))
 
+timebuckets <- function(data) {
+    list(points = c(0,1,60,60*60,24*60*60,7*24*60*60,30*24*60*60,365*24*60*60,max(data)),
+         names = c("Off","Seconds","Minutes","Hours","Days","Weeks","Months","Years"))
+}
+
+allhard <- sparqlfile("all-hard.rq")
+fresh <-  allhard$results$fresh
+fresh[fresh <= 0] <- 0
+hardhist <- hist(fresh, breaks=timebuckets(fresh)$points, plot=F)
+bphard <- barplot(hardhist$count, col="white", names.arg=timebuckets(fresh)$name, xlab="Standards-compliant freshness lifetime", ylab="Frequency", main='')
+text(bphard, hardhist$counts, labels=hardhist$counts, pos=1)
